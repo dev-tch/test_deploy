8eb74b237813834ff51e2c876adce30ca109a9ea
dev-tch <tijeni.chaabeni@gmail.com>
2024-07-04 01:42:59 +0000

    test06

--- tcnproject/urls.py
+++ tcnproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name="home"),
     path('tcn/', include('django.contrib.auth.urls')),
     path("tcn/", include("tcn.urls")),
 ]
